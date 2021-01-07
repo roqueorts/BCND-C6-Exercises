f53e51d13b3e96f8034cc21267adfa2599cedaa7
Roque <roqueorts@gmail.com>
2021-01-07 13:33:56 +0100

    Exercises 1 and 2

--- contracts/ExerciseC6A.sol
+++ contracts/ExerciseC6A.sol
@@ -1,21 +1,20 @@
 pragma solidity ^0.4.25;
 
 contract ExerciseC6A {
-
     /********************************************************************************************/
     /*                                       DATA VARIABLES                                     */
     /********************************************************************************************/
-
+    bool private operational = true;
 
     struct UserProfile {
         bool isRegistered;
         bool isAdmin;
     }
 
-    address private contractOwner;                  // Account used to deploy contract
-    mapping(address => UserProfile) userProfiles;   // Mapping for storing user profiles
-
-
+    address private contractOwner; // Account used to deploy contract
+    mapping(address => UserProfile) userProfiles; // Mapping for storing user profiles
+    uint256 constant M = 2;
+    address[] multiCalls = new address[](0);
 
     /********************************************************************************************/
     /*                                       EVENT DEFINITIONS                                  */
@@ -24,14 +23,10 @@ contract ExerciseC6A {
     // No events
 
     /**
-    * @dev Constructor
-    *      The deploying account becomes contractOwner
-    */
-    constructor
-                                (
-                                ) 
-                                public 
-    {
+     * @dev Constructor
+     *      The deploying account becomes contractOwner
+     */
+    constructor() public {
         contractOwner = msg.sender;
     }
 
@@ -43,53 +38,96 @@ contract ExerciseC6A {
     // before a function is allowed to be executed.
 
     /**
-    * @dev Modifier that requires the "ContractOwner" account to be the function caller
-    */
-    modifier requireContractOwner()
-    {
+     * @dev Modifier that requires the "ContractOwner" account to be the function caller
+     */
+    modifier requireContractOwner() {
         require(msg.sender == contractOwner, "Caller is not contract owner");
         _;
+    }
+
+    /**
+     * @dev Modifier that requires the "operational" boolean variable to be "true"
+     *      This is used on all state changing functions to pause the contract in
+     *      the event there is an issue that needs to be fixed
+     */
+    modifier requireIsOperational() {
+        require(operational, "Contract is currently not operational");
+        _; // All modifiers require an "_" which indicates where the function body will be added
     }
 
     /********************************************************************************************/
     /*                                       UTILITY FUNCTIONS                                  */
     /********************************************************************************************/
 
-   /**
-    * @dev Check if a user is registered
-    *
-    * @return A bool that indicates if the user is registered
-    */   
-    function isUserRegistered
-                            (
-                                address account
-                            )
-                            external
-                            view
-                            returns(bool)
-    {
+    /**
+     * @dev Check if a user is registered
+     *
+     * @return A bool that indicates if the user is registered
+     */
+
+    function isUserRegistered(address account) external view returns (bool) {
         require(account != address(0), "'account' must be a valid address.");
         return userProfiles[account].isRegistered;
+    }
+
+    /**
+     * @dev Get operating status of contract
+     *
+     * @return A bool that is the current operating status
+     */
+
+    function isOperational() public view returns (bool) {
+        return operational;
     }
 
     /********************************************************************************************/
     /*                                     SMART CONTRACT FUNCTIONS                             */
     /********************************************************************************************/
 
-    function registerUser
-                                (
-                                    address account,
-                                    bool isAdmin
-                                )
-                                external
-                                requireContractOwner
+    function registerUser(address account, bool isAdmin)
+        external
+        requireContractOwner
+        requireIsOperational
     {
-        require(!userProfiles[account].isRegistered, "User is already registered.");
+        require(
+            !userProfiles[account].isRegistered,
+            "User is already registered."
+        );
 
         userProfiles[account] = UserProfile({
-                                                isRegistered: true,
-                                                isAdmin: isAdmin
-                                            });
+            isRegistered: true,
+            isAdmin: isAdmin
+        });
+    }
+
+    /**
+     * @dev Sets contract operations on/off
+     *
+     * When operational mode is disabled, all write transactions except for this one will fail
+     */
+
+    function setOperatingStatus(
+        bool mode // requireContractOwner
+    ) external {
+        require(
+            mode != operational,
+            "New mode must be different from existing mode"
+        );
+        require(userProfiles[msg.sender].isAdmin, "Caller is not an admin");
+
+        bool isDuplicate = false;
+        for (uint256 c = 0; c < multiCalls.length; c++) {
+            if (multiCalls[c] == msg.sender) {
+                isDuplicate = true;
+                break;
+            }
+        }
+        require(!isDuplicate, "Caller has already called this function.");
+
+        multiCalls.push(msg.sender);
+        if (multiCalls.length >= M) {
+            operational = mode;
+            multiCalls = new address[](0);
+        }
     }
 }
-
--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
 
 // Be sure to match this mnemonic with that in Ganache!
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+var mnemonic = "intact bread flight prepare extend damp cup wire paper dwarf tourist decide";
 
 module.exports = {
   networks: {
@@ -10,7 +10,7 @@ module.exports = {
         return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 10);
       },
       network_id: '*',
-      gas: 9999999
+     // gas: 9999999
     }
   },
   // Configure your compilers
